e7a7ea8c39593774a657948ffe7f12adb1e2aad0
D20A2614 educacion <wanderssff12@gmail.com>
2022-05-11 00:32:28 -0400

    Actualizacion

---
+++ js/lightbox.js
@@ -0,0 +1,40 @@
+const imagenes=document.querySelectorAll('.img-galeria')
+const imagenesLight=document.querySelector('.agregar-imagen')
+const contenedorLight=document.querySelector('.imagen-light')
+const hamburger1 =document.querySelector('.hamburger');
+
+imagenes.forEach(imagen=>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+
+contenedorLight.addEventListener('click',(e)=>{
+    if(e.target !==imagenesLight){
+        contenedorLight.classList.toggle('show')
+      imagenesLight.classList.toggle('showImage')
+      hamburger1.style.opacity='1'
+    }
+})
+
+const aparecerImagen=(imagen)=>{
+    imagenesLight.src=imagen;
+    contenedorLight.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+    hamburger1.style.opacity='0'
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/menu.js
@@ -0,0 +1,20 @@
+
+const hamburger =document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navegacion');
+
+//console.log(menu);
+//console.log(hamburger);
+
+hamburger.addEventListener('click',()=>{
+    menu.classList.toggle("spread")
+
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread')
+        && e.target !=menu && e.target !=hamburger   ){
+
+            menu.classList.toggle("spread")
+
+    }
+})
